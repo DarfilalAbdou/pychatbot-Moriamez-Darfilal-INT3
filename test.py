20d974a30bdf0e098cd89e7da781f96205a1f2cb
DarfilalAbdou <151127458+DarfilalAbdou@users.noreply.github.com>
2023-11-20 13:58:32 +0100

    Prototype of the TF-IDF matrix funtion

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from fonctionNomPres import *
 from functions import *
-def idf(fichier):
+def TF(fichier):
     f=open("cleaned/"+fichier, "r", encoding="utf-8")
     lines=f.readline()
     lines= lines.split(" ")
@@ -12,25 +12,25 @@ def idf(fichier):
             else:
                 occurence[i] += 1
     return occurence
-#print(idf())
+#print(TF())
 
 
 def matrix(directory):
     list_fichier = list_of_files(directory,".txt")
     tab2 = []
-    print(list_fichier)
+    #Loop to take every text in a given file
     for k in list_fichier :
         fichier = k
         tab=[]
-        for j in idf(fichier).keys():
-            for i in IDF_scores(list_of_speeches_words()).keys():
+        """ We search the key in IF and IDF_scores 
+        so that we can multiply their values when the key are the same
+        """
+        for j in TF(fichier).keys():
+            for i in IDF_scores("./cleaned").keys():
                 if j == i:
-                    tab.append((j,idf(fichier)[j]*IDF_scores(list_of_speeches_words())[i]))
+                    #Creation of the list of the form [word, TF-TDF score]
+                    tab.append((j,TF(fichier)[j]*IDF_scores("./cleaned")[i]))
+        #Creation of the matrix
         tab2.append(tab)
     return tab2
 print(matrix("./cleaned"))
-print(IDF_scores(list_of_speeches_words()))
-
-dico={"orange":2}
-
-print(dico["orange"])
